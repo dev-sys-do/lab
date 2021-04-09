6ca505d19c1ac342aa96af1b36d941bbbee8985c
Samuel Ortiz <sameo@linux.intel.com>
2021-04-09 11:15:34 +0200

    threads: multi: Add some comments
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- threads/multi/src/main.rs
+++ threads/multi/src/main.rs
@@ -2,18 +2,24 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
+    // Thread messages
     let messages = vec!["I'm the first thread!", "I'm the second thread!"];
+
+    // Thread vector, tracking all created threads
     let mut handles = Vec::new();
 
     for message in messages {
+        // Each thread is going to own its message
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_secs(1));
             println!("{}", message);
         });
 
+        // Add the created thread to the thread vector.
         handles.push(handle);
     }
 
+    // Now wait for all threads to terminate.
     for handle in handles {
         handle.join().unwrap();
     }
