2e710d577a0c242dad3f7e5440ef9ceef27df9f4
Samuel Ortiz <sameo@linux.intel.com>
2021-05-28 00:55:36 +0200

    networking: pima: Move all thread handling into a single routine
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- networking/pima/src/main.rs
+++ networking/pima/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use std::io::Read;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::thread::JoinHandle;
 
@@ -45,6 +45,24 @@ fn http_response(req_data: &[u8]) -> std::result::Result<Response, PimaError> {
     Ok(response)
 }
 
+fn handle_http_connection(mut stream: &TcpStream) -> std::result::Result<(), PimaError> {
+    let mut stream_data = [0; 1024];
+
+    println!(
+        "New client {:?}",
+        stream.peer_addr().map_err(PimaError::IoError)?
+    );
+
+    stream.read(&mut stream_data).map_err(PimaError::IoError)?;
+
+    let response = http_response(&stream_data)?;
+    response
+        .write_all(&mut stream)
+        .map_err(PimaError::IoError)?;
+
+    Ok(())
+}
+
 fn main() -> std::result::Result<(), PimaError> {
     let server = TcpListener::bind(format!("{}:{}", DEFAULT_IP, DEFAULT_PORT))
         .map_err(PimaError::IoError)?;
@@ -55,25 +73,10 @@ fn main() -> std::result::Result<(), PimaError> {
     );
 
     for stream in server.incoming() {
-        let mut stream = stream.map_err(PimaError::IoError)?;
+        let stream = stream.map_err(PimaError::IoError)?;
 
-        let _: JoinHandle<std::result::Result<(), PimaError>> = thread::spawn(move || {
-            let mut client_data = [0; 1024];
-
-            println!(
-                "New client {:?}",
-                stream.peer_addr().map_err(PimaError::IoError)?
-            );
-
-            stream.read(&mut client_data).map_err(PimaError::IoError)?;
-
-            let response = http_response(&client_data)?;
-            response
-                .write_all(&mut stream)
-                .map_err(PimaError::IoError)?;
-
-            Ok(())
-        });
+        let _: JoinHandle<std::result::Result<(), PimaError>> =
+            thread::spawn(move || handle_http_connection(&stream));
     }
 
     Ok(())
