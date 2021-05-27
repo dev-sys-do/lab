0ad1ded1871c40ae31af10837990600bb22b5fd6
Samuel Ortiz <sameo@linux.intel.com>
2021-05-28 00:30:07 +0200

    networking: pima: Handle GET requests
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- networking/pima/src/main.rs
+++ networking/pima/src/main.rs
@@ -2,20 +2,48 @@ use std::fs;
 use std::io::Read;
 use std::net::TcpListener;
 
-use micro_http::{Body, Response, StatusCode, Version};
+use micro_http::{Body, Method, Request, RequestError, Response, StatusCode, Version};
 
 #[derive(Debug)]
 pub enum PimaError {
     IoError(std::io::Error),
+
+    HttpRequestError(RequestError),
 }
 
 const DEFAULT_PORT: u16 = 8080;
 const DEFAULT_IP: &str = "0.0.0.0";
 const DEFAULT_SERVER: &str = "pima 0.1";
 const DEFAULT_404: &str = "/tmp/pima/404.html";
+const DEFAULT_STATIC_HTML: &str = "/tmp/pima/static";
+
+fn http_response(req_data: &[u8]) -> std::result::Result<Response, PimaError> {
+    let not_found = Body::new(fs::read_to_string(DEFAULT_404).map_err(PimaError::IoError)?);
+    let request = Request::try_from(req_data).map_err(PimaError::HttpRequestError)?;
+
+    let path = request.uri().get_abs_path();
+    let method = request.method();
+
+    println!("HTTP request: method [{:?}] path [{}]", method, path);
+
+    let (status, body) = match method {
+        Method::Get => fs::read_to_string(format!("{}/{}", DEFAULT_STATIC_HTML, path))
+            .map_or((StatusCode::NotFound, Some(not_found)), |f| {
+                (StatusCode::OK, Some(Body::new(f)))
+            }),
+        _ => (StatusCode::MethodNotAllowed, None),
+    };
+
+    let mut response = Response::new(Version::Http11, status);
+    response.set_server(DEFAULT_SERVER);
+    if let Some(body) = body {
+        response.set_body(body);
+    }
+
+    Ok(response)
+}
 
 fn main() -> std::result::Result<(), PimaError> {
-    let not_found = Body::new(fs::read_to_string(DEFAULT_404).map_err(PimaError::IoError)?);
     let server = TcpListener::bind(format!("{}:{}", DEFAULT_IP, DEFAULT_PORT))
         .map_err(PimaError::IoError)?;
 
@@ -33,11 +61,9 @@ fn main() -> std::result::Result<(), PimaError> {
             stream.peer_addr().map_err(PimaError::IoError)?
         );
 
-        let mut response = Response::new(Version::Http11, StatusCode::NotFound);
-        response.set_server(DEFAULT_SERVER);
-        response.set_body(not_found.clone());
-
         stream.read(&mut client_data).map_err(PimaError::IoError)?;
+
+        let response = http_response(&client_data)?;
         response
             .write_all(&mut stream)
             .map_err(PimaError::IoError)?;
